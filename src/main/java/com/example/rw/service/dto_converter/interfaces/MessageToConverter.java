0b714bd05a3949e74766a3d53776a384312b01dc
Kirill E <cmakreal7@mail.ru>
2024-02-08 17:39:26 +0300

    fix: make mapper converters manually override methods

--- src/main/java/com/example/rw/service/dto_converter/interfaces/MessageToConverter.java
+++ src/main/java/com/example/rw/service/dto_converter/interfaces/MessageToConverter.java
@@ -1,5 +1,6 @@
 package com.example.rw.service.dto_converter.interfaces;
 
+import com.example.rw.exception.model.dto_converting.ToConvertingException;
 import com.example.rw.model.dto.message.MessageRequestTo;
 import com.example.rw.model.dto.message.MessageResponseTo;
 import com.example.rw.model.entity.implementations.Message;
@@ -7,4 +8,10 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface MessageToConverter extends ToConverter<Message, MessageRequestTo, MessageResponseTo> {
+
+    @Override
+    Message convertToEntity(MessageRequestTo requestTo) throws ToConvertingException;
+
+    @Override
+    MessageResponseTo convertToDto(Message entity) throws ToConvertingException;
 }
--- src/main/java/com/example/rw/service/dto_converter/interfaces/NewsToConverter.java
+++ src/main/java/com/example/rw/service/dto_converter/interfaces/NewsToConverter.java
@@ -1,5 +1,6 @@
 package com.example.rw.service.dto_converter.interfaces;
 
+import com.example.rw.exception.model.dto_converting.ToConvertingException;
 import com.example.rw.model.dto.news.NewsRequestTo;
 import com.example.rw.model.dto.news.NewsResponseTo;
 import com.example.rw.model.entity.implementations.News;
@@ -7,4 +8,10 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface NewsToConverter extends ToConverter<News, NewsRequestTo, NewsResponseTo> {
+
+    @Override
+    News convertToEntity(NewsRequestTo requestTo) throws ToConvertingException;
+
+    @Override
+    NewsResponseTo convertToDto(News entity) throws ToConvertingException;
 }
